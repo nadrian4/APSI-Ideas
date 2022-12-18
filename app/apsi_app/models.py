de48215fb19efb3db8200e2332c6f9452537c379
Alexander Załęski <01142601@pw.edu.pl>
2022-12-18 13:18:30 +0100

    ocenianie i podział na strony

--- app/apsi_app/models.py
+++ app/apsi_app/models.py
@@ -7,7 +7,14 @@ class Pomysl(models.Model):
     tytul = models.CharField(max_length=20)
     tresc = models.CharField(max_length=200)
     data = models.DateField(default=timezone.now)
+    srednia_ocen = models.IntegerField()
     uzytkownik = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    
     def __str__(self):
         return self.tytul
+
+
+class Ocena(models.Model):
+    ocena = models.IntegerField()
+    pomysl = models.ForeignKey(Pomysl, on_delete=models.CASCADE)
+    uzytkownik = models.ForeignKey(User, on_delete=models.CASCADE)
--- app/apsi_app/views.py
+++ app/apsi_app/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 
-from .models import Pomysl
+from .models import Ocena, Pomysl
 
 
 def index(request):
+    if request.method == 'POST':
+        ocena_pomyslu = request.POST['ocena']
+        pomysl = Pomysl.objects.get(pk=request.POST['pomysl'])
+        ocena = Ocena(ocena=ocena_pomyslu, pomysl=pomysl, uzytkownik=request.user)
+        ocena.save()
+        
+        oceny = Ocena.objects.filter(pomysl=pomysl)
+        srednia_ocen = 0
+
+        for o in oceny:
+            srednia_ocen += o.ocena
+
+        srednia_ocen = srednia_ocen / len(oceny)
+        pomysl.srednia_ocen = srednia_ocen * 20
+        pomysl.save()
+
+    paginator = Paginator(Pomysl.objects.all(), 5)
+    page = request.GET.get('page')
+    pomysly = paginator.get_page(page)
+
     context = {
-        'pomysly': Pomysl.objects.all()
+        'pomysly': pomysly,
+        'pomysly_liczba': range(1, paginator.num_pages+1),
+        'page': page
     }
 
     return render(request, 'apsi_app/index.html', context)
