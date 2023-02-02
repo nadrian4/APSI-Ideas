6fd5d92ca45123feaca4f3db891817115a11e3f8
Alexander Załęski <47083235+olek123456789@users.noreply.github.com>
2023-02-02 17:19:55 +0100

    Revert "Glosowanie pojedyncze wartosci"

--- app/apsi_app/admin.py
+++ app/apsi_app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from apsi_app.models import Konkurs, Glosowanie, Ocena, Pomysl, Komentarz, Glos
+from apsi_app.models import Konkurs, Glosowanie, Ocena, Pomysl, Komentarz
 
 admin.site.register(Konkurs)
 admin.site.register(Pomysl)
 admin.site.register(Ocena)
 admin.site.register(Glosowanie)
 admin.site.register(Komentarz)
-admin.site.register(Glos)
