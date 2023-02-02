087178ca8dd8fcf78a02073867ce208f63bb33d4
Alexander Załęski <01142601@pw.edu.pl>
2023-02-02 17:01:39 +0100

    planowane korzyści i koszty, słowa kluczowe

--- app/apsi_app/forms.py
+++ app/apsi_app/forms.py
@@ -7,10 +7,12 @@ from django.contrib.auth.models import User
 class PomyslForm(forms.ModelForm):
     tytul = forms.CharField(max_length=20)
     tresc = forms.CharField(max_length=200)
+    planowane_korzysci = forms.CharField(max_length=200)
+    planowane_koszty = forms.FloatField()
     kategoria = forms.ChoiceField(choices=KATEGORIE)
     kto_moze_oceniac = forms.ChoiceField(choices=ROLE)
     plik = forms.ImageField(required=False)
 
     class Meta:
         model = Pomysl
-        fields = ['tytul', 'tresc', 'kategoria', 'kto_moze_oceniac', 'plik']
+        fields = ['tytul', 'tresc', 'planowane_korzysci', 'planowane_koszty', 'kategoria', 'kto_moze_oceniac', 'plik']
