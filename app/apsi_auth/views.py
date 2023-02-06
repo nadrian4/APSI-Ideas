6bfc2c8cd2696e2e5921b8bd9253f0e8ea7ee744
Alexander Załęski <01142601@pw.edu.pl>
2023-02-07 00:27:11 +0100

    poprawki wyglądu

--- app/apsi_auth/views.py
+++ app/apsi_auth/views.py
@@ -22,16 +22,3 @@ def login_apsi(request):
         login_form = LoginForm()
 
     return render(request, 'apsi_auth/login.html', {'login_form': login_form})
-
-    # if request.method == 'POST':
-    #     uzytkownik = request.POST['uzytkownik']
-    #     haslo = request.POST['haslo']
-    #     konto = authenticate(request, username=uzytkownik, password=haslo)
-
-    #     if konto is not None:
-    #         login(request, konto)
-    #         return redirect('index')
-    #     else:
-    #         return redirect('login')
-    # else:
-    #     return render(request, 'apsi_auth/login.html', {})
